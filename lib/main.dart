595435112c6231a34411b9de8e96d7a11ef84e34
a7md0 <10885603+a7md0@users.noreply.github.com>
2021-11-13 12:47:45 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Unhandled Ex Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Unhandled Exception Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int? _statusCode;
+
+  Future<void> _incrementCounter() async {
+    try {
+      var httpClient = Dio()..options.contentType = "application/json";
+
+      var response = await httpClient.get(
+        "https://gorest.co.in/public/v1/posts/nonExistingId",
+      );
+
+      setState(() {
+        _statusCode = response.statusCode;
+      });
+    } on DioError catch (dioError) {
+      setState(() {
+        _statusCode = dioError.response?.statusCode;
+      });
+
+      print(dioError.message);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'API response status code:',
+            ),
+            Text(
+              '${_statusCode ?? "-"}',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.api),
+      ),
+    );
+  }
+}
